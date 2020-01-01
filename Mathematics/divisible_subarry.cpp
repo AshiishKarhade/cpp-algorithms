ccf1c8b1f3d09a5160251c65f4e83678f05a1961
Ashish Karhade <ashishkarhade@Ashishs-MacBook-Air.local>
2020-01-01 12:19:55 +0530

    div_subarr

--- Mathematics/divisible_subarry.cpp
+++ Mathematics/divisible_subarry.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
-#include <"/Users/ashishkarhade/Documents/Developer/Coding\ Blocks/stdc++.h">
+#include <cstring>
+using namespace std;
+#define ll long long
 
-int main(){
-    return 0;
+ll arr[1000005], prefixSum[1000005];
+
+int main() {
+    int T; //Test cases
+    cin >> T;
+
+    while(T--){
+        int n; //size of array
+        cin >> n;
+
+        ll sum = 0;
+        memset(prefixSum, 0, sizeof(prefixSum)); // initializes the elements of prefixSum to '0' upto the sizeof(prefixSum) i.e all elements is initialised with 0
+        prefixSum[0] = 1;
+
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+            sum += arr[i];
+            sum %= n;
+            sum = (sum+n) % n;
+            prefixSum[sum]++; 
+        }
+
+        ll ans = 0;
+        for(int i=0; i<n; i++){
+            ll no = prefixSum[i];
+            ans += ((no)*(no-1))/2;
+        }
+        cout << ans << endl;
+
+    }
+
 }
+/*
+Input:
+2
+5
+1 1 1 1 1
+5
+5 5 5 5 5
+
+Output:
+1
+15
+*/
